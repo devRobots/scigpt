9c79f4f35399c7702071add232ea7e974ee5c338
DevRobot <ysrosast@gmail.com>
2024-07-22 15:00:49 +0200

    Review page advancement

--- app/writer/[uuid]/review/page.tsx
+++ app/writer/[uuid]/review/page.tsx
@@ -1,25 +1,44 @@
 'use client';
 
-import { usePapers } from '@/app/hooks/usePapers';
 import PaperList from '@/app/components/writer/PapersReviews';
-import { useSearchParams } from 'next/navigation';
+import { usePapers } from '@/app/hooks/usePapers';
+import { updateDraft } from '@/app/lib/firebase/firestore';
+import { Button } from '@nextui-org/button';
+import { useParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function References() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get('query') || 'lorem ipsum';
-  const fieldOfStudy = searchParams.get('fieldOfStudy') || 'Computer Science';
+  const router = useRouter();
+  const params = useParams();
+  const uuid = params.uuid.toString() || '';
 
   const { papers, loading, getPapers } = usePapers();
 
   useEffect(() => {
+    const query = 'Hola';
     getPapers(query);
   }, []);
 
+  const handleNext = () => {
+    if (papers.length < 3) return;
+
+    updateDraft(uuid, {
+      papers: papers,
+      stage: 'method'
+    }).then(() => {
+      router.push(`/writer/${uuid}/method`);
+    });
+  };
+
   return (
-    <main className="p-8">
-      <h1 className="editorial-header mb-8">Papers</h1>
-      <div className="columns-2">
+    <main className="flex flex-col p-8">
+      <div className="flex flex-row w-full justify-between">
+        <h1 className="editorial-header mb-8">Papers</h1>
+        <Button className="super-button" onClick={handleNext}>
+          Next
+        </Button>
+      </div>
+      <div className="flex flex-col gap-3">
         {loading ? <p>Loading...</p> : <PaperList papers={papers} />}
       </div>
     </main>
