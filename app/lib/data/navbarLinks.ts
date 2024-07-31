4254eb9eacf0147f6de6201add38a1d0190acbdb
DevRobot <ysrosast@gmail.com>
2024-07-31 21:45:49 +0200

    clean: navbar links data clean up

--- app/lib/data/navbarLinks.ts
+++
@@ -1,5 +0,0 @@
-export const links = [
-    { id: 'index', name: 'Inicio', path: '/' },
-    { id: 'editor', name: 'Editor', path: '/writer' },
-    { id: 'about', name: 'Acerca', path: '/about' }
-  ];
