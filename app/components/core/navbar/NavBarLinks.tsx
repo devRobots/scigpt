522b058cff1d6b7f92d9ea913d29a12d31570d70
DevRobot <ysrosast@gmail.com>
2024-08-01 23:42:38 +0200

    feature: interactive navbar links

--- app/components/core/navbar/NavBarLinks.tsx
+++ app/components/core/navbar/NavBarLinks.tsx
@@ -1,26 +1,42 @@
+'use client';
+
 import { Link } from '@nextui-org/link';
 import { NavbarItem, NavbarMenuItem } from '@nextui-org/navbar';
-import { FaHouse, FaFeather, FaInfo } from 'react-icons/fa6';
+import { usePathname } from 'next/navigation';
+import { FaFeather, FaHouse, FaInfo } from 'react-icons/fa6';
+
+function isPath(path: string, pathname: string) {
+  console.log(path, pathname);
+  if (pathname === '/') return path === '/';
+  return path.startsWith(pathname);
+}
+
+function menu(path: string, pathname: string) {
+  if (isPath(path, pathname)) return 'text-primary';
+  else return 'text-white';
+}
 
 export default function NavBarLinks({ isMenu }: { isMenu?: boolean }) {
+  const path = usePathname();
   const ItemType = isMenu ? NavbarMenuItem : NavbarItem;
+  console.log(path);
 
   return (
     <>
       <ItemType key="navbaritem-0" className="justify-items-center">
-        <Link className="text-white" href="/" size="lg">
+        <Link className={menu(path, '/')} href="/" size="lg">
           <FaHouse className="mr-1" />
           Inicio
         </Link>
       </ItemType>
       <ItemType key="navbaritem-1" className="justify-items-center">
-        <Link className="text-white" href="/app" size="lg">
+        <Link className={menu(path, '/app')} href="/app" size="lg">
           <FaFeather className="mr-1" />
           Editor
         </Link>
       </ItemType>
       <ItemType key="navbaritem-2" className="justify-items-center">
-        <Link className="text-white" href="/about" size="lg">
+        <Link className={menu(path, '/about')} href="/about" size="lg">
           <FaInfo />
           Acerca
         </Link>
